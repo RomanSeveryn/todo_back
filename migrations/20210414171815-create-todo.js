b0166fe9cdcbaaa10f43523eef001f226a97ea37
RomanSeveryn <romaseverin50@gmail.com>
2021-04-14 20:45:09 +0300

    Add model todo

---
+++ migrations/20210414171815-create-todo.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      deadline: {
+        type: Sequelize.DATEONLY,
+      },
+      isDone: {
+        field: 'is_done',
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('todos');
+  },
+};
---
+++ models/todo.js
@@ -0,0 +1,43 @@
+'use strict';
+const { Model } = require('sequelize');
+const { isAfter } = require('date-fns');
+
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    static associate (models) {}
+  }
+  Todo.init(
+    {
+      body: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      deadline: {
+        type: DataTypes.DATEONLY,
+        validate: {
+          isDate: true,
+          isValidDate (value) {
+            if (isAfter(new Date(value), new Date())) {
+              throw new Error('Please, check day');
+            }
+          },
+        },
+      },
+      isDone: {
+        field: 'is_done',
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Todo',
+      tableName: 'todo',
+      underscored: true,
+    }
+  );
+  return Todo;
+};
