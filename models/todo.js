5215f8d304903fbd3d4695f255558f3fb1c4b24d
RomanSeveryn <romaseverin50@gmail.com>
2021-05-21 16:56:00 +0300

    Fixed model todo

--- models/todo.js
+++ models/todo.js
@@ -12,7 +12,6 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.STRING,
         validate: {
-          isAlphanumeric: true,
           notNull: true,
           notEmpty: true,
         },
